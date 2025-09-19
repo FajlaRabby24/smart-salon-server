db82ef5bcdca0e06ff6e65a31fca75e3f291b843
Fajla Rabby <fajlarabbyla24@gmail.com>
2025-09-19 09:42:54 +0600

    remove mongoose and add mongodb

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -10,18 +10,30 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-// database connection with mongoose
-mongoose
-  .connect(process.env.MONODB_URL)
-  .then(() => console.log("DB connection successfully"))
-  .catch((err) => console.log(err));
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONODB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your DB.");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hello world");
-});
-
-app.get("/hello", (req, res) => {
-  res.send("this is new route");
 });
 
 app.listen(PORT, () => {
