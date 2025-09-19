9fcf095ce6665a41ae3e139a3907ac83d64d1652
zaheenrakib <rakibzaheen@gmail.com>
2025-09-19 10:54:04 +0600

    update

--- models/User.js
+++ models/User.js
@@ -1,9 +1,53 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  phone: { type: String, required: true, unique: true },
-  role: { type: String, enum: ["customer", "admin"], default: "customer" }
-}, { timestamps: true });
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: null,
+  },
+  address: {
+    type: String,
+    default: null,
+  },
+  city: {
+    type: String,
+    default: null,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function(v) {
+        return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: 'Please enter a valid email'
+    }
+  },
+  phone: {
+    type: String,
+    default: null,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "saloon", "admin"],
+    default: "user",
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active",
+  }
+}, {
+  timestamps: true
+});
 
 module.exports = mongoose.model("User", userSchema);
